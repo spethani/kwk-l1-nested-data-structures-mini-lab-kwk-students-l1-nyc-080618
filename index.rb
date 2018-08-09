cd464ab0362fb50fc6e46d4594c9e831227db1d3
spethani <sarahpethani@gmail.com>
2018-08-09 15:22:53 +0000

    Done.

--- index.rb
+++ index.rb
@@ -8,6 +8,31 @@ ingredients = [
 
 def sandwich_request(ingredients, order) 
   # your code here!
+  is_it_there=true
+  
+  order.each do |item|
+    
+    #Set a variable found equal to false, means order item not found in the ingredients array yet
+    found=false
+    
+    ingredients.each do |array|
+      array.each do |ingredient|
+        #if the item is the same as the ingredient in the ingredients array, you have found the item
+        if(item==ingredient)
+          found=true
+        end
+        
+      end
+    end
+    
+    if(!found)
+      is_it_there=false
+    end
+    
+  end
+  
+  return is_it_there
+  
 end
 
 # example city_info hash
@@ -36,4 +61,12 @@ cities = {
 
 def city_populations(cities)
   # your code here!
+  total_pop = 0
+  cities.each do |city, info|
+    total_pop += info[:population]
+  end
+  
+  return total_pop
 end
+
+puts city_populations(cities)
